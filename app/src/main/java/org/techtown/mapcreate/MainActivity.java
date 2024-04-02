2e9d918f500dc1f33807d4c8de483b7da4185d84
Pimcol <qkrwnsah4643@gmail.com>
2024-04-02 11:15:19 +0900

    Google Map

--- app/src/main/java/org/techtown/mapcreate/MainActivity.java
+++ app/src/main/java/org/techtown/mapcreate/MainActivity.java
@@ -3,12 +3,22 @@ package org.techtown.mapcreate;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+        mapFragment.getMapAsync(this);  // 지도 콜백에 등록
+    }
+
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(37.7382831, 128.8766612);
+        googleMap.addMarker(new MarkerOptions().position(location).title("가톨릭관동대학교"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12));
+        // CameraUpdate를 사용해 카메라 이동할 위치 지정 (즉, 카메라 위치 변경)
+        // CameraUpdateFactory를 사용해, 여러 유형의 CameraUpdate 만들 수 있다.
     }
 }
